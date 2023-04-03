802eb7d1ec5f5e7ea7762d1432b869fc456f6751
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-04-02 21:15:56 -0400

    working on register/login

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -12,16 +12,17 @@ function Register() {
         <h2> Register </h2>
         <div>
           <label htmlFor="name"> Name: </label>
-          <input type="text" onChange={(e)=> setUserData({ ...userData, name: e.target.value})} />
+          <input type="text" value={userData.name} onChange={(e)=> setUserData({ ...userData, name: e.target.value})} />
         </div>
         <div>
           <label htmlFor="email"> Email: </label>
-          <input type="text" onChange={(e)=> setUserData({ ...userData, email: e.target.value})} />
+          <input type="text" value={userData.email} onChange={(e)=> setUserData({ ...userData, email: e.target.value})} />
         </div>
         <div>
           <label htmlFor="password"> Password: </label>
-          <input type="text" onChange={(e)=> setUserData({ ...userData, password: e.target.value})} />
+          <input type="text" value={userData.password} onChange={(e)=> setUserData({ ...userData, password: e.target.value})} />
         </div>
+        <button type="submit">Register</button>
       </form>
     </div>
   )
