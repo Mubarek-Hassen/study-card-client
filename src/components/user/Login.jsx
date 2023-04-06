c543f0a66ea8c52f8d2dd4ccc0cca6e5f54c65c0
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-04-06 14:14:04 -0400

    cookies are introduced

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,11 +1,29 @@
+import axios from "axios"
 import { useState } from "react"
+import { useCookies } from "react-cookie"
+import { useNavigate } from "react-router-dom"
 function Login() {
   const [userData, setUserData] = useState({
     email: "",
     password: ""
   })
 
-  
+  const navigate = useNavigate()
+
+  const [_,setCookies] = useCookies(["access_token"])
+  const onLogin = async (event)=>{
+    event.preventDefault()
+
+    try {
+      const response = axios.post("http://localhost:8080/user/login", userData)
+      setCookies("access_token", response.data.token)
+      window.localStorage.setItem("userID", response.data.userId)
+      navigate("/")
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div>
       <form>
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,11 +1,11 @@
+import Login from "../components/user/Login";
 import Register from "../components/user/Register";
 
 function User() {
   return (
     <div>
       <Register />
-      {/* <Login />
-      <Register /> */}
+      <Login />
     </div>
   )
 }
