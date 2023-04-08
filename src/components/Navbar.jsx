f2ef7a9eba34ab3b541d8fb7d68ab41ea382c8a6
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-04-07 23:27:23 -0400

    register needs fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useCookies } from "react-cookie"
 function Navbar() {
+  const [cookies, setCookies] = useCookies(["access_token"])
+  const navigate = useNavigate()
+  const logout =()=>{
+    setCookies("access_token", "");
+    window.localStorage.removeItem("userId");
+    navigate("/user")
+  }
   return (
     <div className="green">
       <Link to="/"> Home </Link>
       <Link to="/create-card"> Create Card </Link>
-      <Link to="/user"> Login/Register </Link>
+      {!cookies.access_token ? <Link to="/user"> Login/Register </Link> : <button onClick={ logout }>Logout</button>}
     </div>
   )
 }
