1fa697466fe1446908a3996b1e24d639005c3dc5
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-04-08 21:16:43 -0400

    working on dynamic login or register page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,11 +1,18 @@
 import Login from "../components/user/Login";
 import Register from "../components/user/Register";
-
+import { useState } from "react";
 function User() {
+  const [isRegister, setIsRegister] = useState(false)
+  const sign =()=>{
+    setIsRegister(!isRegister)
+  }
   return (
     <div>
-      <Register />
-      <Login />
+
+      {!isRegister && <Register />}
+      {isRegister && <Login />}
+      <h5>Already have an account?</h5>
+      <button onClick={sign}>Login</button>
     </div>
   )
 }
