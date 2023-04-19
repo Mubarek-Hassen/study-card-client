d945077a3732599670294cf12c9fe839377cee87
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-04-18 21:29:07 -0400

    working on input changes

--- src/pages/CreateCard.jsx
+++ src/pages/CreateCard.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react"
 
 function CreateCard() {
+  const [card, setCard] = useState({
+    front: "",
+    back: "",
+  });
+
+  const inputChange = (event)=>{
+    
+    // setCard({ event.target.name: event.target.value })
+  }
   return (
     <div>
       <h2>Create Card</h2>
       <form>
-        <label htmlFor="front">Front: </label>
+        <label htmlFor="front" name="front" >Front: </label>
         <input type="text" />
         <br />
-        <label htmlFor="front">Back: </label>
+        <label htmlFor="back" name="back" >Back: </label>
         <input type="textarea" />
       </form>
       </div>
