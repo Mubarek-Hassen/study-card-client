84ab87adf0f5e1a349b51f4b88392e98a5dc9171
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2024-10-02 20:26:11 -0400

    login register switch

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,21 +1,22 @@
-import { Box, Button, Input,  Typography } from "@mui/material";
-import { Form, useActionData, useSearchParams } from "react-router-dom";
+import { Box, Button,  Input, Typography } from "@mui/material";
+import { Form, Link, useActionData, useSearchParams } from "react-router-dom";
 
 const AuthForm = () => {
 const [searchParams] = useSearchParams()
-const mode = searchParams.get("mode");
+const isLogin = searchParams.get("mode") === "login";
 const errors= useActionData()
 console.log(errors);
 
   return (
     <Box>
-      <Typography variant="h4">{mode==="login" ? "Login":"Register"}</Typography>
-      <Form style={{display: "flex", flexDirection: "column", maxWidth: "40rem", margin: "0 auto", gap: "1.5rem"}}>
-        {/* <TextField label="Email"  name="email" sx={{color: "violet"}}/>
-        <TextField label="password" type="password"  name="email" sx={{color: "violet"}}/> */}
+      <Typography variant="h4">{isLogin ? "Login":"Register"}</Typography>
+      <Form method="post" style={{display: "flex", flexDirection: "column", maxWidth: "40rem", margin: "0 auto", gap: "1.5rem"}}>
         <Input placeholder="email" name="email" />
+        {!isLogin && <Input name="name" placeholder="name" />}
         <Input placeholder="password" name="password" type="password" />
         <Button variant="contained" type="submit" sx={{bgcolor: "black", ":hover": {bgcolor: "green"} }}>Submit</Button>
+        <Typography>OR</Typography>
+        <Link to={`?mode=${isLogin ? "register" : "login"}`} >Create an account</Link>
       </Form>
     </Box>
   )
--- src/utils/router-actions/auth-actions.ts
+++ src/utils/router-actions/auth-actions.ts
@@ -5,29 +5,31 @@ export async function authAction({request}: ActionFunctionArgs) {
   console.log(request)
   const searchParams = new URL(request.url).searchParams;
   const mode = searchParams.get("mode")
-  const data = await request.formData()
-  const url = import.meta.env.BASE_URL
+  const url = import.meta.env.VITE_BASE_URL
   console.log(mode);
+  console.log(url)
   
+  const data = await request.formData()
 
   const enteredData = {
     email: data.get("email"),
     password: data.get("password"),
   }
-
-  console.log(url)
   console.log(data);
+  console.log(enteredData);
 
-  const res = await fetch(`${url}user/${mode}`,  {
+
+  const res = await fetch(`${url}user/${mode}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify(enteredData),
   })
-  const resData = await res.json()
-
   console.log(res)
+
+  const resData = await res.json()
   console.log(resData);
+
 
 }
